e701dce248b5e6795ba656c844d0c7fb7a0a67d6
Pranab  Gubhaju <59060737+Daskhas@users.noreply.github.com>
2023-03-29 19:57:26 +0545

    assignment done

--- to-be-completed/To-do-list.sol
+++ to-be-completed/To-do-list.sol
@@ -8,13 +8,23 @@ contract Todo{
     Choose the appropriate variable type for each field.
 
     */
-  
+    struct task{
+        uint256 id;
+        string title;
+        bool completed;
+    }
+    
     ///Create a counter to keep track of added tasks
+    uint256 counter;  
 
     /*
     create a mapping that maps the counter created above with the struct taskcount
     key should of type integer
     */
+    mapping(uint256 => task) taskcount;
+
+  
+
    
     /*
     Define a constructor
@@ -23,16 +33,27 @@ contract Todo{
     Set the counter to  zero
     */
     
+    constructor() public{
+        owner = msg.sender;
+        counter = 0;
+    }
+
     /*
 
     Define 2 events
     taskadded should provide information about the title of the task and the id of the task
     taskcompleted should provide information about task status and the id of the task
     */ 
+    event taskadded (string title,uint256 id);
+    event taskcompleted(bool completed, uint id);
     
  /*
         Create a modifier that throws an error if the msg.sender is not the owner.
     */
+    modifier onlyOwner(){
+        require(msg.sender == owner ,"Only owner can access");
+        _;
+    }
     
 
     /*
@@ -42,17 +63,27 @@ contract Todo{
     Be sure to check :
     taskadded event is emitted
      */
+     function addTask(string memory _title) public{
+        taskcount[counter] = task(counter, _title, false);
+        emit taskadded(_title, counter);
+        counter++;
+     }
 
     
 
     /*Define a function  to get total number of task added in this contract*/
-    
+    function getTotalTasks() public view returns(uint256) {
+        return counter;
+    }
 
     /**
     Define a function gettask()
     This function takes 1 argument ,task id and 
     returns the task name ,task id and status of the task
      */
+     function gettask(uint256 _taskId) public view returns(string memory,uint256,bool){
+         return (taskcount[_taskId].title, taskcount[_taskId].id, taskcount[_taskId].completed);
+     }
     
     /**Define a function marktaskcompleted()
     This function takes 1 argument , task id and 
@@ -60,5 +91,10 @@ contract Todo{
     Be sure to check:
     taskcompleted event is emitted
      */
+    function markTaskCompleted(uint256 _taskId) public onlyOwner {
+        require(taskcount[_taskId].id == _taskId, "Task does not exist");
+        taskcount[_taskId].completed = true;
+        emit taskcompleted(true, _taskId);
+    }
     
 }
--- to-be-completed/Vote.sol
+++ to-be-completed/Vote.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier:MIT
 pragma solidity 0.8.10;
 
 contract cityPoll {
@@ -9,32 +10,48 @@ contract cityPoll {
     }
 
 
-    mapping() public cities; //mapping city Id with the City ctruct - cityId should be uint256
-    mapping() hasVoted; //mapping to check if the address/account has voted or not
+    mapping(uint256=>City) public cities; //mapping city Id with the City struct - cityId should be uint256
+    mapping(address=>bool) hasVoted; //mapping to check if the address/account has voted or not
+
+    modifier voted(){
+        require(hasVoted[msg.sender]==false,"Already voted");
+        _;
+    }
 
     address owner;
     uint256 public cityCount = 0; // number of city added
-    constructor() public {
-    
-    //TODO set contract caller as owner
-    //TODO set some intitial cities.
-    }
- 
- 
-    function addCity() public {
-      //  TODO: add city to the CityStruct
-
-    }
-    
-    function voteCity() public {
+        constructor() public {
         
+        //TODO set contract caller as owner
+        //TODO set some intitial cities.
+            owner = msg.sender;
+            addCity("Lalitpur");
+            addCity("Kathmandu");
+            addCity("Bhaktapur");
+        }
+    
+ 
+ 
+    function addCity(string memory _cityName) public {
+      //  TODO: add city to the CityStruct
+      cityCount++;
+      cities[cityCount] = City(_cityName,0);
+    }
+    
+    function voteCity(uint256 _cityId) public voted{
         //TODO Vote the selected city through cityID
+        cities[_cityId].vote++;
+        hasVoted[msg.sender] = true;
 
     }
-    function getCity() public view returns (string memory) {
+
+    function getCity(uint256 _cityId) public view returns (string memory) {
      // TODO get the city details through cityID
+     return cities[_cityId].cityName;
     }
-    function getVote() public view returns (uint256) {
+
+    function getVote(uint256 _cityId) public view returns (uint256) {
     // TODO get the vote of the city with its ID
+        return cities[_cityId].vote;
     }
 }
